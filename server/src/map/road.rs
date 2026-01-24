dc4c294bcf13ef54a58123604c68429f3cd6c657
mjoannon <mjoannon@enssat.fr>
2026-01-24 21:52:30 +0100

    refactor Roads and Map logic

--- server/src/map/road.rs
+++ server/src/map/road.rs
@@ -3,15 +3,9 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Road {
     pub id: u32,
-    pub road_type: RoadType,
-    pub lanes: u32,
-    pub max_speed_kmh: f32,
+    pub lane_count: u8,
+    pub speed_limit_kmh: u8,
     pub length_m: f32,
     pub is_blocked: bool,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub enum RoadType {
-    Bilateral,
-    Unilateral,
+    pub can_overtake: bool,
 }
