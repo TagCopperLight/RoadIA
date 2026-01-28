ac12381066b3aaa69b2d046c4c596ea1ecef25b0
TagCopperLight <tagcopperlight@gmail.com>
2026-01-28 15:47:59 +0100

    Unified all websockets files into one

--- client/app/websocket/websocket_sender.tsx
+++
@@ -1,9 +0,0 @@
-import {WebsocketManager} from "./websocket_manager";
-
-const websocketManager:WebsocketManager = new WebsocketManager();
-
-// Envoie un token au serveur prouvant l'identité du client
-export function sendConnectionToken(token:String){
-    const data = {Token:token};
-    websocketManager.send("connect", data);
-}
