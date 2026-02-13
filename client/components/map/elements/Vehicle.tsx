4f287e5d3fe732dfc6d1f44e7e8ac96e1169740e
TagCopperLight <tagcopperlight@gmail.com>
2026-02-13 07:30:08 +0100

    Vehicles showing on right lane

--- client/components/map/elements/Vehicle.tsx
+++ client/components/map/elements/Vehicle.tsx
@@ -8,7 +8,19 @@ export function Vehicle({ data }: VehicleProps) {
     return (
         <pixiGraphics draw={(graphics) => {
             graphics.clear();
-            graphics.position.set(data.x, data.y);
+            
+            let x = data.x;
+            let y = data.y;
+
+            if (data.heading !== undefined) {
+                const offsetDistance = 4;
+                const offsetX = offsetDistance * Math.cos(data.heading + Math.PI / 2);
+                const offsetY = offsetDistance * Math.sin(data.heading + Math.PI / 2);
+                x += offsetX;
+                y += offsetY;
+            }
+
+            graphics.position.set(x, y);
             graphics.setFillStyle({ color: 'purple' });
             graphics.circle(0, 0, 5);
             graphics.fill();
