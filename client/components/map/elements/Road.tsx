f847dbfd5758d6683410578b0fc12c5ce07a67f8
Alexandre <athibaul@enssat.fr>
2026-03-12 15:27:19 +0100

    Add dashed lines

--- client/components/map/elements/Road.tsx
+++ client/components/map/elements/Road.tsx
@@ -1,8 +1,11 @@
 import { MapNode } from '../types';
+import {Graphics} from "pixi.js";
+
+const laneWidth = 7;
+const lineWidth = 1;
+const lineLength = 8;
 
 export function Road({ start, end, laneCount }: { start: MapNode, end: MapNode, laneCount: number }) {
-	const laneWidth = 7;
-	const lineWidth = 1;
 	const width = (laneWidth + lineWidth) * 2 * laneCount - lineWidth;
 	return (
 		<pixiGraphics draw={(graphics) => {
@@ -20,13 +23,29 @@ export function Road({ start, end, laneCount }: { start: MapNode, end: MapNode, 
 			graphics.rect(0, -width / 2, length, width);
 			graphics.fill();
 
-			for (let line = -laneCount+1; line < laneCount; line++){
-				const linePosition = line*(laneWidth+lineWidth);
-				graphics.setStrokeStyle({ color: 'white' });
-				graphics.moveTo(0, linePosition);
-				graphics.lineTo(length, linePosition);
-				graphics.stroke({width: lineWidth});
+			graphics.setStrokeStyle({ color: 'white' });
+			graphics.moveTo(0, 0);
+			graphics.lineTo(length, 0);
+			graphics.stroke({width: lineWidth});
+
+			for (let line = -1; line > -laneCount; line--){
+				drawDashedLine(graphics, line, length);
+			}
+			for (let line = 1; line < laneCount; line++){
+				drawDashedLine(graphics, line, length);
 			}
 		}} />
 	);
+}
+
+function drawDashedLine(graphics: Graphics, line: number, length: number){
+	const linePosition = line*(laneWidth+lineWidth);
+	graphics.setStrokeStyle({ color: 'white' });
+
+	for(let dash = lineLength; dash + lineLength < length; dash += lineLength*2){
+		graphics.moveTo(dash, linePosition);
+		graphics.lineTo(dash + lineLength, linePosition);
+	}
+
+	graphics.stroke({width: lineWidth});
 }
