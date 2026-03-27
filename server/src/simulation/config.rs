60546104229c396e6b9460a65d222096d7ac8d29
TagCopperLight <tagcopperlight@gmail.com>
2026-03-27 19:44:11 +0100

    New intersection system

--- server/src/simulation/config.rs
+++ server/src/simulation/config.rs
@@ -11,3 +11,9 @@ pub struct SimulationConfig {
 
 pub const MAX_SPEED: f32 = 42.0;
 pub const ACCELERATION_EXPONENT: f32 = 4.0;
+
+pub const LOOK_AHEAD: f32 = 0.1;
+pub const STOP_DWELL_TIME: f32 = 1.0;
+pub const IMPATIENCE_RATE: f32 = 0.05;
+pub const MIN_CREEP_SPEED: f32 = 1.0;
+pub const LANE_WIDTH: f32 = 7.5;
