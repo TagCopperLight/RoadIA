4d0a22bc810675d31ed0e2e8ae8fbaea8412f8b6
TagCopperLight <tagcopperlight@gmail.com>
2026-05-05 19:01:40 +0200

    fix: linting

--- client/components/Toolbar.tsx
+++ client/components/Toolbar.tsx
@@ -78,7 +78,7 @@ function IconModeSimulation() {
     );
 }
 
-function IconStatistics() {
+function _IconStatistics() {
     return (
         <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
             <path d="M18 20V10" strokeLinecap="round" strokeLinejoin="round"/>
